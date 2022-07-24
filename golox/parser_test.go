68958f70660bf974cd9423e6b5a75ff20ca06a46
Shane Lillie <ignignokterr@gmail.com>
2022-07-24 14:43:46 -0700

    Parser test

--- golox/parser_test.go
+++ golox/parser_test.go
@@ -1,3 +1,57 @@
 package main
 
-// TODO:
+import (
+	"testing"
+)
+
+func TestBinaryExpression(t *testing.T) {
+	// -123 * (45.67)
+	expectedResult := "(* (- 123) (group 45.67))"
+
+	tokens := []*Token{
+		{
+			Type:   Minus,
+			Lexeme: "-",
+			Line:   1,
+		},
+		{
+			Type:    Number,
+			Lexeme:  "123",
+			Literal: NewNumberLiteral(123),
+			Line:    1,
+		},
+		{
+			Type:   Star,
+			Lexeme: "*",
+			Line:   1,
+		},
+		{
+			Type:   LeftParen,
+			Lexeme: "(",
+			Line:   1,
+		},
+		{
+			Type:    Number,
+			Lexeme:  "45.67",
+			Literal: NewNumberLiteral(45.67),
+			Line:    1,
+		},
+		{
+			Type:   RightParen,
+			Lexeme: ")",
+			Line:   1,
+		},
+		{
+			Type: EOF,
+			Line: 1,
+		},
+	}
+
+	parser := NewParser(tokens)
+	expression := parser.Parse()
+
+	result := (&ExpressionPrinter{}).Print(expression)
+	if result != expectedResult {
+		t.Fatalf("Print failed - expected %s, got %s", expectedResult, result)
+	}
+}
