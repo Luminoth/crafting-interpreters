a4d795680e6cb4eb2727aac68be4cd911e5c2f4a
Shane Lillie <ignignokterr@gmail.com>
2022-08-08 19:55:28 -0700

    "configurable" native print function

--- golox/callable.go
+++ golox/callable.go
@@ -1,12 +1,26 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type Callable interface {
 	Call(interpreter *Interpreter, arguments []Value) (Value, error)
 }
 
 /* Native functions */
+
+type PrintFunction struct {
+}
+
+func (f *PrintFunction) Call(interpreter *Interpreter, arguments []Value) (Value, error) {
+	fmt.Println(arguments[0])
+
+	// no return value here
+	// because it looks weird to print things twice
+	return NewNilValue(), nil
+}
 
 type ClockFunction struct {
 }
