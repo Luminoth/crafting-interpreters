bb0aaf6ff3b5430eeb2fbf04e0d244188cedc7a7
Shane Lillie <ignignokterr@gmail.com>
2022-08-23 19:11:15 -0700

    Bump tester to chapter 12

--- test-golox.sh
+++ test-golox.sh
@@ -12,4 +12,5 @@ cd -
 echo "Running tests ..."
 cd craftinginterpreters
 #dart tool/bin/test.dart jlox --interpreter ../golox/golox
-dart tool/bin/test.dart chap11_resolving --interpreter ../golox/golox
+#dart tool/bin/test.dart clox --interpreter ../loxrs/loxrs
+dart tool/bin/test.dart chap12_classes --interpreter ../golox/golox
