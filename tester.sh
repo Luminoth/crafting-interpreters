351a5b6a9057570e2929bb091e23b5e43fbca09d
Shane Lillie <ignignokterr@gmail.com>
2022-09-01 18:46:25 -0700

    Cleanup tester a bit

--- tester.sh
+++ tester.sh
@@ -1,16 +1,36 @@
 #! /bin/sh
 
+GO=go
+CARGO=cargo
+
+JLOX=golox
+CLOX=loxrs
+
 echo "Running generator ..."
 ./generate-ast.py generate
 
-echo "Building golox ..."
-cd golox/
-go build
+echo "Running jlox tests ..."
 
+echo "Building $JLOX ..."
+cd $JLOX/
+$GO build
 cd -
 
-echo "Running tests ..."
 cd craftinginterpreters
-dart tool/bin/test.dart jlox --interpreter ../golox/golox
-#dart tool/bin/test.dart clox --interpreter ../loxrs/loxrs
-#dart tool/bin/test.dart chap13_inheritance --interpreter ../golox/golox
+dart tool/bin/test.dart jlox --interpreter ../$JLOX/$JLOX
+cd -
+
+echo
+
+echo "Running clox tests ..."
+
+echo "Building $CLOX ..."
+cd $CLOX/
+$CARGO build
+cp target/debug/$CLOX .
+cd -
+
+cd craftinginterpreters
+#dart tool/bin/test.dart clox --interpreter ../$CLOX/$CLOX
+dart tool/bin/test.dart chap17_compiling --interpreter ../$CLOX/$CLOX
+cd -
